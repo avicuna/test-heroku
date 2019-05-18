3953f9b8aacfe2d5a9d9f0f6c041c2341560c2dc
alexvicuna <vicunaalejandro@gmail.com>
2019-05-18 16:46:37 -0400

    added api endpoint

--- src/main/java/com/alexvicuna/testherokuspring/TestHerokuSpringApplication.java
+++ src/main/java/com/alexvicuna/testherokuspring/TestHerokuSpringApplication.java
@@ -2,9 +2,17 @@ package com.alexvicuna.testherokuspring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 public class TestHerokuSpringApplication {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home(){
+        return "Hello World!";
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestHerokuSpringApplication.class, args);

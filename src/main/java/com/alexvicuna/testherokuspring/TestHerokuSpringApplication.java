4608147d493a1cc47cb105d782c19fb3bab3c334
alexvicuna <vicunaalejandro@gmail.com>
2019-05-19 12:08:28 -0400

    Changed returned string

--- src/main/java/com/alexvicuna/testherokuspring/TestHerokuSpringApplication.java
+++ src/main/java/com/alexvicuna/testherokuspring/TestHerokuSpringApplication.java
@@ -13,7 +13,7 @@ public class TestHerokuSpringApplication {
     @RequestMapping("/")
     @ResponseBody
     String home(){
-        return "Hello World!";
+        return "Hello Everyone!";
     }
 
 	public static void main(String[] args) {
